984f14abd7d3a8ceff049663b9a95c8cee5ac4ce
ClaudianyMartins <41713981+ClaudianyMartins@users.noreply.github.com>
2020-09-28 12:38:48 -0300

    Initial commit

---
+++ src/Home/home.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { confirmAlert } from "react-confirm-alert"; // Import
+import "react-confirm-alert/src/react-confirm-alert.css"; // Import css
+import "./home.css";
+
+export default class Home extends Component {
+  direcionarTelaMenorIdade() {
+    this.history.push("/telamenoridade"); // falta  fazer o router
+  }
+  direcionarTelaMaiorIdade() {
+    this.history.push("/telamaioridade"); // falta  fazer o router
+  }
+
+  componentDidMount() {
+    this.carregarConfirmDialog();
+  }
+
+  carregarConfirmDialog = () => {
+    confirmAlert({
+      title: "Cervejaria lalalla",
+      message: "Você é maior de idade?",
+      buttons: [
+        {
+          label: "Sim",
+          onClick: () => this.direcionarTela()
+        },
+        {
+          label: "Não",
+          onClick: () => this.direcionarTelaMaiorIdade()
+        }
+      ]
+    });
+  };
+  render() {
+    return <div className="container"></div>;
+  }
+}
